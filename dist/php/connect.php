a9da91a7c4ca615b9eed627ffc6034e2fc7e346f
Lex Jauregui <lextefasu@gmail.com>
2025-03-03 11:05:53 -0600

    Cambios varios

--- dist/php/connect.php
+++ dist/php/connect.php
@@ -1,17 +1,18 @@
 <?php
-
-$hostname = '161.35.13.96';
+//error_log('hola');
+$hostname = '173.201.179.213'; //'161.35.13.96'; //173.201.179.213
 //$hostname = 'localhost';
-$username = 'dreamtech';
+$username = 'crm-root';//'dreamtech'; //crm-root
 $password = 'mqAla.jzjC9';
-$db_name  = 'jc-proyect';
+$db_name  = 'crm_base'; //'jc-proyect';
 
 $conn = new mysqli($hostname, $username, $password, $db_name);
 
 if($conn->connect_error){
+	//error_log("clavo:".$conn->connect_error);
 	die("Connection failed: ".$conn->connect_error);
 }else{
-	//echo "LOGRADO!";
+	//error_log("LOGRADO!") ;
 }
 
 ?>
